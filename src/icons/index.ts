9a5337665236ec0aed18166ebe4b97df8c5880df
kanak035 <deepsaurabh4362@gmail.com>
2026-01-17 20:08:26 +0530

    feat: add Facebook icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,4 +3,5 @@ export * from './ui/Search';
 export * from './ui/User';
 export * from './social/Github';
 export * from './social/Twitter';
+export * from './social/Facebook';
 export * from './IconBase';
