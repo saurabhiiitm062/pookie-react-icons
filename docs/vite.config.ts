a978c39113ab8417c76ac850bd703ba9fef52996
kanak035 <deepsaurabh4362@gmail.com>
2026-01-17 21:35:42 +0530

    congif

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            'pookie-react-icons': path.resolve(__dirname, '../src/index.ts'),
-        },
+  base: '/pookie-react-icons/', 
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'pookie-react-icons': path.resolve(__dirname, '../src/index.ts'),
     },
+  },
 });
